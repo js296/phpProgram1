693c6a84d413550a08d0cc230b93d131100e9085
js296 <js296@njit.edu>
2017-01-31 17:35:40 -0500

    made a person OOP index.php

--- index.php
+++ index.php
@@ -1,14 +1,48 @@
 <?php
-
-  $obj = new main;
+  $obj = new main();
 
   class main{
+    public function __construct (){
+      echo 'Program has begun. <br>';
+      $person = personFactory::create();
 
-    public function __construct(){
+      $person->jump();
+      $person->sit();
 
-      echo 'hello world!';
+      print_r($person);
+   }
+    public function __destruct (){
+      echo ' Program has finished. <br>';
+    }
+  }
+
+  class personFactory{
+    public static function create(){
+      $person = new person;
+      return $person;
+    }
+  }
+
+  abstract  class personAction{
+    public function jump(){
+      echo 'is jumping <br>';
     }
 
+    public function sit(){
+      echo 'is sitting down <br>';
+    }
+  }
+
+  class person extends personAction{
+    public $name;
+    public $gender;
+    public $age;
+
+    public function __construct(){
+      $this->name = 'Jason Sevilla';
+      $this->gender = 'M';
+      $this->age = '25';
+    }
   }
 
 ?>
